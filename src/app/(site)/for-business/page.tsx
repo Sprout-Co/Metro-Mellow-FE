3a3d8f02b197859a0ed472a71451e31b5681e248
randdom_addvice <akoladefaruq42@gmail.com>
2025-07-15 22:43:36 +0100

    feat: add BusinessBenefits component to ForBusiness page for enhanced business offerings

--- src/app/(site)/for-business/page.tsx
+++ src/app/(site)/for-business/page.tsx
@@ -2,12 +2,14 @@
 
 import React from "react";
 import BusinessHero from "./_components/BusinessHero/BusinessHero";
+import BusinessBenefits from "./_components/BusinessBenefits/BusinessBenefits";
 import styles from "./ForBusiness.module.scss";
 
 export default function BusinessPage() {
   return (
     <main className={styles.forBusiness}>
       <BusinessHero />
+      <BusinessBenefits />
       {/* Other business sections will be added here */}
     </main>
   );
