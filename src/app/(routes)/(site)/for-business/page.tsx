cfcfcd9f9e0f767df930f42fc788fd449df8ed96
randdom_addvice <akoladefaruq42@gmail.com>
2025-07-15 23:39:37 +0100

    feat: add EnterpriseFeatures component to ForBusiness page to enhance service offerings

--- src/app/(routes)/(site)/for-business/page.tsx
+++ src/app/(routes)/(site)/for-business/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import BusinessHero from "./_components/BusinessHero/BusinessHero";
 import BusinessBenefits from "./_components/BusinessBenefits/BusinessBenefits";
 import EnterpriseServices from "./_components/EnterpriseServices/EnterpriseServices";
+import EnterpriseFeatures from "./_components/EnterpriseFeatures/EnterpriseFeatures";
 import SuccessStoriesSection from "./_components/SuccessStoriesSection/SuccessStoriesSection";
 import styles from "./ForBusiness.module.scss";
 
@@ -13,6 +14,7 @@ export default function BusinessPage() {
       <BusinessHero />
       <BusinessBenefits />
       <EnterpriseServices />
+      <EnterpriseFeatures />
       <SuccessStoriesSection />
       {/* Other business sections will be added here */}
     </main>
