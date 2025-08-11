76c49a926e78e358cba0ec401735fea73bf7a3e0
randdom_addvice <akoladefaruq42@gmail.com>
2025-08-11 23:40:58 +0100

    feat: integrate new Notification feature in DashboardLayout to enhance user engagement and streamline updates

---
+++ src/app/(routes)/(app)/dashboard/bookings/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DashboardLayout from "../_components/DashboardLayout/DashboardLayout";
+
+const BookingsPage = () => {
+  return (
+    <DashboardLayout>
+      <div>BookingsPage</div>
+    </DashboardLayout>
+  );
+};
+
+export default BookingsPage;
